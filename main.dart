c31ee4814ce5e596ce68f1fb17e453b02c5ef2c2
meenubemara <meenubemara@gmail.com>
2025-06-26 22:44:03 +0530

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,13 @@
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Bill Splitter',
+      home: HomePage(),
+    );
+  }
+}
